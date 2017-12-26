bde9dcb8363a505dd5a317853a712032cd53c23d
Harry Wynn-Williams <harry.wynn-williams@ladbrokescoral.com>
2017-12-26 11:34:47 +0000

    added cell 5 logo

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={require('./cell-5-logo.jpg')} className="App-logo" alt="logo" />
+          <h1 className="App-title">Cell 5</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                </p>
       </div>
     );
   }
