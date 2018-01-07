3c2865c569848c1125ce5e24e4061bf93fb47926
Harry Wynn-Williams <harry.wynn-williams@ladbrokescoral.com>
2018-01-07 13:02:58 +0000

    bolding stuff on bios

--- src/App.js
+++ src/App.js
@@ -76,15 +76,15 @@ class App extends Component {
             coach. Initially trained at Thoughtworks and going on to a career Goldman Sachs building trading systems,
             architectures, and teams, Peter has strong fundamentals as well as years of experience in Java, Javascript,
              Scala, Clojure etc. Peter met the rest of team while working as an independent contractor as an hands on
-             architect and XP Coach. Peter helps guide architecture and get the team unstuck. He has lots of recent
-             experience automating CI/CD so he helps the team on getting to prod early and often.</p>
+             architect and XP Coach. <b>Peter helps guide architecture and get the team unstuck. He has lots of recent
+             experience automating CI/CD so he helps the team on getting to prod early and often.</b></p>
               <p><b>Meet Harry.</b>  He was a derivatives trader and entrepreneur with a keen interest in finance and
             technology before retraining as a developer. He has delivered with Peter on two client products as a
             full stack developer working on Java, Clojure and Javascript. He is currently especially keen on ReactJs
             and front end development and builds websites in his free time. In our last project he mastered the end
             to end system and was who the team called on to debug issues or get things working, he puts in extra
-            hours to further his passion. Harry is the team hacker,  debugger, fixer. He is often the first to blaze
-             a path into the unknown,  and he knows how to get things done and keep things simple.</p>
+            hours to further his passion. <b>Harry is the team hacker,  debugger, fixer. He is often the first to blaze
+             a path into the unknown,  and he knows how to get things done and keep things simple.</b></p>
             </div>
             <div>
               <h2>Recent Tech Stack:</h2>
