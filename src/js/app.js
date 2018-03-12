d99e619ad8c705eb3ec4a3bef9d8ed27574e05ce
Pat Bautista <patzj006@gmail.com>
2018-03-12 15:56:48 +0800

    Fix tagline section for mobile

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,26 @@
 
 $(document).ready(function() {
     // To animate tagline
-    $("#rotating-text").Morphext({
-        animation: "fadeIn",
-        separator: ",",
-        speed: 15000,
-        complete: function () {
-            setTimeout(function() {
-                var active = $('.tagline-text-wrapper ul li.active');
-                active.removeClass('active');
-                if(active.next().length) {
-                    active.next().addClass('active');
-                } else {
-                    $('.tagline-text-wrapper ul li:first-child').addClass('active');
-                }
-            }, 15000)
-        }
-    });
+    setTimeout(function() {
+        $('#rotating-text').text('web, mobile, design, cloud, social media, s.e.o., legacy code uplift, start-up growth');
+        $('.tagline-text-wrapper ul li:first-child').addClass('active');
+        $('#rotating-text').Morphext({
+            animation: "fadeIn",
+            separator: ",",
+            speed: 7500,
+            complete: function () {
+                setTimeout(function() {
+                    var active = $('.tagline-text-wrapper ul li.active');
+                    active.removeClass('active');
+                    if(active.next().length) {
+                        active.next().addClass('active');
+                    } else {
+                        $('.tagline-text-wrapper ul li:first-child').addClass('active');
+                    }
+                }, 7500)
+            }
+        });
+    }, 15000);
 
     // To close nav dropdown when a nav link is clicked
     $('header .nav-link').on('click', animateSectionScroll);
