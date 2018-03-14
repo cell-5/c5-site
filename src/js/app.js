f82cdc78fefbf17515c0b5de06a8750ec03544f2
Pat Bautista <patzj006@gmail.com>
2018-03-15 00:19:46 +0800

    Services draft

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,50 @@
 'use strict';
 
 $(document).ready(function() {
+    // Services
+    $('.services-carousel').slick({
+        slidesToShow: 3,
+        arrows: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '0',
+        focusOnSelect: true,
+        asNavFor: '.services-carousel-active',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.services-carousel-active').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.services-carousel'
+    });
+
     // To animate tagline
     setTimeout(function() {
         $('#rotating-text').text('web, mobile, design, cloud, social media, s.e.o., legacy code uplift, start-up growth');
@@ -8,7 +52,7 @@ $(document).ready(function() {
         $('#rotating-text').Morphext({
             animation: "fadeIn",
             separator: ",",
-            speed: 7500,
+            speed: 15000,
             complete: function () {
                 setTimeout(function() {
                     var active = $('.tagline-text-wrapper ul li.active');
@@ -18,17 +62,16 @@ $(document).ready(function() {
                     } else {
                         $('.tagline-text-wrapper ul li:first-child').addClass('active');
                     }
-                }, 7500)
+                }, 15000)
             }
         });
     }, 15000);
 
     // To close nav dropdown when a nav link is clicked
     $('header .nav-link').on('click', animateSectionScroll);
-
     $('.cta a').on('click', animateSectionScroll);
-
-    $('.sitefooter-bottom a').on('click', animateSectionScroll);
+    $('#logo a').on('click', animateTopScroll);
+    $('.sitefooter-bottom a').on('click', animateTopScroll);
 
     function animateSectionScroll(e) {
         e.preventDefault();
@@ -43,21 +86,31 @@ $(document).ready(function() {
         }, 500);
     }
 
-    var homeYOffset = $('#tagline').height() + $('#home').height(),
-        testimonialsYOffset = $('#testimonials').offset().top + $('#testimonials').height(),
-        profilesYOffset = $('#profiles').offset().top + $('#profiles').height();
+    function animateTopScroll(e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    }
+
+    var servicesYOffset = Math.floor($('#services').offset().top),
+        testimonialsYOffset = Math.floor($('#testimonials').offset().top),
+        profilesYOffset = Math.floor($('#profiles').offset().top),
+        contactUsYOffset = Math.floor($('#contact-us').offset().top);
 
     $(window).on('scroll', function() {
-        var yOffset = window.pageYOffset;
-        var childNum = 4;
+        var yOffset = window.pageYOffset,
+            childNum = 5;
 
         // To change active nav link
-        if(yOffset <= homeYOffset) {
+        if (yOffset >= 0 && yOffset < servicesYOffset) {
             childNum = 1;
-        } else if(yOffset <= testimonialsYOffset) {
+        } else if (yOffset >= servicesYOffset && yOffset < testimonialsYOffset) {
             childNum = 2;
-        } else if(yOffset <= profilesYOffset) {
+        } else if (yOffset >= testimonialsYOffset && yOffset < profilesYOffset) {
             childNum = 3;
+        } else if (yOffset >= profilesYOffset && yOffset < contactUsYOffset) {
+            childNum = 4;
         }
 
         $('header .nav-item.active').removeClass('active');
