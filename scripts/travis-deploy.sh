2f33fbaa6e3f8762109e10586a156d76c0516962
-peterabarry <-peter@everve.com>
2018-03-18 12:28:00 +0000

    try different key

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.aws
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${aws_access_key_id}
+aws_secret_access_key = ${aws_secret_access_key}
 EOL
-echo ${AWS_ACCESS_KEY_ID}
+echo "key id ${aws_access_key_id}"
 ~/.local/bin/aws s3 sync build s3://cell5.co.uk --region=eu-west-1 --delete --cache-control="max-age=31536000"
 ~/.local/bin/aws s3 cp build/index.html s3://cell5.co.uk --cache-control "max-age=0, public"
