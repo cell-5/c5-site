e2e7262244e29594f14b892d3dbf80daf5c94655
-peterabarry <-peter@everve.com>
2018-03-22 21:42:32 +0000

    test

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,2 +1,2 @@
 ~/.local/bin/aws s3 sync build s3://cell5.co.uk --region=eu-west-1 --delete --cache-control="max-age=31536000"
-~/.local/bin/aws s3 cp build/index.html s3://cell5.co.uk --region=eu-west-1 --delete --cache-control "max-age=0, public"
+~/.local/bin/aws s3 cp build/index.html s3://cell5.co.uk --region=eu-west-1 --cache-control="max-age=0, public"
