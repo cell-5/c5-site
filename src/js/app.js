65a061fc5c8f01cddf008d4cd29d79b2af12b5e8
Rex Darel Andig <rexdarel@vimlabs.com>
2018-05-12 23:13:02 +0800

    added testimonials

--- src/js/app.js
+++ src/js/app.js
@@ -96,6 +96,42 @@ $(document).ready(function() {
         focusOnSelect: true
     });
 
+    $('.carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
     // Contact form submission
     var form = $('#contact-form')
     ,   name = $('#name')
