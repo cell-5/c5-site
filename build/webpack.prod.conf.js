756563b21188e55f7be088c6a8c2cfb029427c44
jedi gueruela <jgueruela@gmail.com>
2018-06-04 12:17:50 +0800

    Add SEO tools and favicon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -97,6 +97,9 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      }, {
+        from: path.resolve(__dirname, '../seo'),
+        to: config.build.assetsRoot
       }
     ])
   ]
