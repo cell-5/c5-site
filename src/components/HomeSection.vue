95664bfefc9218e717fa5128f007902935de8c15
rexdarel <rexdarel.andig@g.msuiit.edu.ph>
2018-06-08 15:42:05 +0800

    added img from the poster

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -42,7 +42,7 @@
 
 <style scoped>
   #home {
-    background-image:url('../assets/img/banner.jpg');
+    background-image:url('../assets/img/header.jpg');
     background-size:cover;
     background-color: #2c302e;
     color: #fefffe;
