2f83d1b57afad1f07f7aba3aaa81da4d5f6e6116
allan <allandoblon@gmail.com>
2019-01-02 22:00:01 +0800

    Update PortfolioSection.vue

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -3,28 +3,22 @@
     <Header></Header>
     <main role="main">
       <section id="portfolio-heading">
-        <div class="container text-center">
-          <div class="col-md-7">
-            <h1>PORTFOLIO</h1>
-            This is a sample content of the section
-          </div>
+        <div class="container">
+          <h1>PORTFOLIO</h1>
+          Here We Gathered Some Of Our Creative Work
         </div>
       </section>
       <section id="portfolio">
         <div class="container">
-          <div class="navbar-nav">
-            <ul class="text-center">
-              <li class="nav-item" v-for="nav in this.portfolioNavigation"><a href="#home">{{ nav }}</a></li>
-            </ul>
-          </div>
           <div class="row justify-content-center align-items-center">
-            <carousel-3d :controls-visible="true" :style="{height:'500px'}" :autoplay="false" :autoplay-timeout="5000"  perspective="0">
-              <slide v-for="(portfolioItem, index) in this.portfolio" :index="index" :key="index">
-                <template slot-scope="{ isCurrent}" >
+            <carousel-3d :controls-visible="true" :width="carouselWidth" v-bind:display="itemCount" :autoplay="true" :autoplay-timeout="5000"  perspective="0">
+              <slide v-for="(portfolioItem, index) in this.portfolio" :index="index" :key="index">                
+                <template slot-scope="{ isCurrent}">
                   <img :data-index="index" :class="{ current: isCurrent }" :src="portfolioItem.image">
                   <div  v-bind:class="{ 'show-description' : isCurrent, 'hide' : !isCurrent}">
                     <h6>{{portfolioItem.title}}</h6>
                     {{portfolioItem.service}}
+                    <div class="link"><a :href="portfolioItem.link">VISIT SITE</a></div>
                   </div>
                 </template>
               </slide>
@@ -35,8 +29,8 @@
       <div class="get-started">
         <div class="container text-center">
           <h1>Get Started Today!</h1>
-          <div>This is a sample content of the section</div>
-          <a href="#contact-us" class="learn-more-btn call-to-action">Subscribe Now</a>
+          <div>We would love to have your website listed above so get in touch and let us impress you.</div>
+          <a href="/#contact-us" class="learn-more-btn">Subscribe Now</a>
         </div>
       </section>
     </main>
@@ -61,7 +55,6 @@ function backToTop() {
     scrollTop: 0
   }, 500)
 }
-
 export default {
   name: 'portfolio',
   data() {
@@ -104,7 +97,9 @@ export default {
       portfolioNavigation: [
         'All','Business','Entertainment','Creative','Blog','Portfolio','One page','Shop','Other' 
       ],
-      cell5logo: Cell5BlackLogo
+      cell5logo: Cell5BlackLogo,
+      itemCount: 0,
+      carouselWidth: 0
     }
   },
   components: {
@@ -113,8 +108,27 @@ export default {
     Carousel3d,
     Slide
   },
+  methods: {
+    handleResize() {
+      let path = this.$route.path;
+      if (window.matchMedia("only screen and (max-width: 768px)").matches) {
+          this.carouselWidth = 200;
+      } else if (window.matchMedia("only screen and (max-width: 1024px)").matches) {
+          this.carouselWidth = 300;
+      } else {
+          this.carouselWidth = 450;
+      }
+      if ( window.matchMedia("only screen and (max-width: 600px)").matches) {
+        this.itemCount = 1;
+      } else {
+        this.itemCount = 5;
+      }
+    }
+  },
   mounted () {
-    backToTop()
+    backToTop(),
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize();
   }
 }
 </script>
@@ -129,10 +143,11 @@ header.siteheader {
 #portfolio-heading {
   color: #fff;
   background: linear-gradient(to right, #232474, #aa245c);
-  padding: 30px 0 40px;
+  padding: 30px 0 40px 150px;
   font-size: 0.7rem;
   letter-spacing: 0.05rem;
   margin: 4.1rem 0;
+  text-align: left;
 }
 #portfolio-heading h1{
   font-weight: bold;
@@ -142,24 +157,24 @@ header.siteheader {
   content: "";
   position: absolute;
   right: 0;
-  top: 90px;
+  top: 100px;
   height: 3.13rem;
   width: 50%;
   border-left: 3.13rem solid transparent;
   border-bottom: 3.13rem solid #fefffe;
 }
-.navbar-nav , .navbar-nav .nav-item {
+.portfolio-navbar , .portfolio-navbar .nav-item {
   padding: 0 10px;
   display: inline;
   font-size: 0.8rem;
   font-weight: bold;
   word-break: break-all;
 }
-.navbar-nav  a {
+.portfolio-navbar  a {
   text-decoration: none;
   color: #000;
 }
-.navbar-nav  a:hover, .navbar-nav  a:active {
+.portfolio-navbar  a:hover, .portfolio-navbar  a:active {
   text-decoration: none;
   color: #e22571;
 }
@@ -170,7 +185,7 @@ a.learn-more-btn:visited {
   color: white;
   border-radius: 5px;
   padding: 0.7rem 3.5rem;
-  margin: 1rem 0 0;
+  margin: 1.8rem 0 0;
   text-decoration: none;
   font-weight: 600;
   font-size: 1rem;
@@ -188,8 +203,11 @@ a.learn-more-btn:active {
 .get-started {
   background: linear-gradient(to right, #11052b, #230930);
   color: #fff;
-  font-size: 1.4rem;
-  padding: 50px 0 70px;
+  font-size: 1rem;
+  padding: 70px 0 60px;
+}
+.carousel-3d-container {
+  height: 620px !important;
 }
 .carousel-3d-slide {
   border:none !important; 
@@ -198,6 +216,9 @@ a.learn-more-btn:active {
 }
 .carousel-3d-slide:not(.current) {
   box-shadow: 0px 15px 60px -20px rgba(0,0,0,1);
+}
+.carousel-3d-slide.current {
+  padding: 0;
 }
 .carousel-3d-slide img {
   box-shadow: 0px 15px 60px -20px rgba(0,0,0,1);
@@ -221,38 +242,74 @@ a.learn-more-btn:active {
   text-align: center;
   padding: 30px 0;
 }
+.show-description .link {
+  margin: 20px 0 0;
+}
+.show-description a {
+  border: 1px solid;
+  font-size: 14px;
+  padding: 8px 20px;
+  text-decoration: none;
+}
+.show-description a:hover {
+  text-decoration: none;
+}
 .carousel-3d-slide .hide {
   display: none;
 }
-
-@media (max-width: 767px) and (orientation: portrait) {
-  .overlay {
-    visibility: hidden;
-    display: none;
-  }
-  .mobile-caption{
-    visibility: visible;
-    display: inline;
-  }
-}
 @media (max-width: 1200px) {
-    .overlay-content h5 {
-      font-size:1rem;
-    }
-    .overlay-content p {
-      font-size: 0.8rem;
-    }
-    .overlay-content img {
-        height: 25px;
-        margin: 0;
-    }
+  #portfolio-heading {
+    padding: 30px 0 30px 80px;
+  }
+  #portfolio-heading:after {
+    top: 80px;
+  }
+  .carousel-3d-container {
+    height: 420px !important;
+  }
+  .carousel-3d-controls .prev, .carousel-3d-controls .next {
+    height: 30px !important;
+    width: 30px !important;
+    line-height: 15px !important;
+    padding: 0 6px;
+    font-size: 40px !important;
+  }
 }
-@media (max-width: 992px) {
-    .overlay-content h5 {
-      font-size: 0.8rem;
-    }
-    .overlay-content p {
-      font-size: 0.6rem;
-    }
+@media (max-width: 991px) {
+  #portfolio-heading {
+    padding: 30px 0;
+    margin: 4.1rem 0 2rem;
+  }
+  #portfolio-heading:after {
+    top: 80px;
+    width: 45%;
+  }
+  .carousel-3d-container {
+    height: 350px !important;
+  }
+}
+@media (max-width: 767px) {
+  .carousel-3d-container {
+    padding: 10px 0;
+  }
+  .carousel-3d-controls .prev, .carousel-3d-controls .next {
+    height: 30px !important;
+    width: 30px !important;
+    line-height: 15px !important;
+    padding: 0 6px;
+    font-size: 40px !important;
+  }
+  #portfolio-heading {
+    padding: 20px 0 30px;
+    margin: 4.1rem 0 1rem;
+    text-align: center;
+  }
+  #portfolio-heading h1{
+    font-weight: bold;
+    margin: 0 0 5px 0;
+  }
+  #portfolio-heading:after {
+    content: none;
+  }
 }
 </style>
